ab3f4c286cfe5ab55d77ccf1437c6219659c27ee
kobe-victor <victor.goulart@kobe.io>
2020-06-15 01:09:05 -0300

    -Setup navigation component -Create tweet data class -Create main activity -Create list fragment -Create map fragment

---
+++ app/src/main/java/com/example/sumtwitter/model/Tweet.kt
@@ -0,0 +1,9 @@
+package com.example.sumtwitter.model
+
+data class Tweet(
+    val userName: String,
+    val userId: String,
+    val text: String,
+    val date: String,
+    val userImage: String
+)
